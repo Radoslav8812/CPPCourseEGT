371ac6e25fb3f8809aa3dc9a9d082905c1f5c5f0
Radoslav Bogdanov <radimk8812@abv.bg>
2024-02-05 17:18:28 +0200

    C++

---
+++ LibraryManagementSystem_EXAM/Task.h
@@ -0,0 +1,30 @@
+
+
+/*
+                Implement a Library Management System
+ 
+    Create C++ program for a simple library management system.
+    The program should include classes for books and library members, and demonstrate the use of objects, methods and pointers.
+ 
+        Requirements:
+            Book class:
+                Attributes:
+                    title(string)
+                    author(string)
+                    ISBN(string)
+            Member class:
+                Attributes:
+                    name(string)
+                    age(int)
+                    memberID(string)
+ 
+            Methods:
+                A constructor to initialize member details.
+                display() to output member details.
+                borrowBook(Book* book) to simulate borrowing a book, which takes a pointer to a Book Obj.
+ 
+            Main Function:
+                Create an array of Book objects and array of Member objects.
+                Demonstrate the borrowing process by having a member borrow a book.
+                Use pointers to refer to and minipulate the book and member objects.
+ */
