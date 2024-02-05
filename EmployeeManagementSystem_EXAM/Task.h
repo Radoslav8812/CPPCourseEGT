89f72e54d27ab3b3e6900b56473ccf5ca8a7d7a9
Radoslav Bogdanov <radimk8812@abv.bg>
2024-02-05 16:30:26 +0200

    C++

---
+++ EmployeeManagementSystem_EXAM/Task.h
@@ -0,0 +1,30 @@
+
+
+
+/*    Implement an Employee Management System
+    
+    Develop a C++ program that models a simple employee management system.
+    This program should include classes for employees and departments, showing the use of objects, methods, and pointers.
+ 
+Requirements:
+    Employee Class:
+        Methods:
+            A consstructor to initialize employee details.
+            method "display()" to output employee details to the console.
+ 
+    Department Class:
+        Attributes:
+            departmentName(string)
+            employees(pointer to an array of employee obj)
+            numEmployees(int to keep track of employees count)
+        Methods:
+            A constructor to initialize the department name.
+            addEmployee (Employee* employee) to add an employee to department
+            displayEmpoyees() to list all employees in the department.
+ 
+ 
+        Main Function:
+            Create several employee and department objects.
+            Add empoyees to the department and display the list of all employees in it.
+ 
+ */
