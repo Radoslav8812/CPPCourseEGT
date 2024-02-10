987e899406b4dea623a9ff1ff0dbcd7a4032cb64
Radoslav Bogdanov <radimk8812@abv.bg>
2024-02-10 19:36:58 +0200

    c++

---
+++ SDL2Mp3Player_Personal_Homework/SDL2Music.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <SDL_mixer.h>
+
+using namespace std;
+
+class SDL2Music
+{
+
+private:
+	vector<Mix_Music*> musicVect;
+
+	bool mPaused = false;
+	bool mPlaying = false;
+
+public:
+
+	SDL2Music();
+	
+	void addMusicTrack(const char* path);
+	void playMusicTrack(const int which);
+	void Play_Pause();
+};
+
